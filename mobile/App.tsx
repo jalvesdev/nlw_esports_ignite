390a8a0e38d48f0e9813f26d57f1fb62b1b7d704
José Alves <oi@jalves.dev.br>
2022-09-14 11:14:40 -0300

    FEAT: basic structure for mobile version

---
+++ mobile/App.tsx
@@ -0,0 +1,42 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, TouchableOpacity,} from 'react-native';
+
+interface ButtonProps {
+  title: string
+}
+
+function Button(props: ButtonProps){
+  return (
+    <TouchableOpacity>
+
+      <Text>
+        {props.title}
+      </Text>
+    </TouchableOpacity>
+
+  )
+}
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}> uau </Text>
+      <Button title="Click me"/>
+      <Button title="Send 1"/>
+      <Button title="hello world!"/>
+    </View>
+  );
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontSize: 22,
+  }
+});

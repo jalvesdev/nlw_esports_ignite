7698b465263835bedd77520a7064cc9ca96ab11b
José Alves <oi@jalves.dev.br>
2022-09-15 14:33:17 -0300

    FEAT: add home and splash screen for Mobile

---
+++ mobile/src/components/Background/styles.ts
@@ -0,0 +1,10 @@
+import { StyleSheet } from 'react-native'
+import { THEME } from '../../theme'
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1, // para que ocupe a tela toda
+    backgroundColor: THEME.COLORS.BACKGROUND_800 // DEFINE A COR DE FUNDO
+  }
+
+})
